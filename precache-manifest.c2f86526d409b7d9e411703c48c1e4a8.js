68f406ef4461bcb3e8fba4d3259ad199a3e4fce2
likejean <popachs@yahoo.com>
2020-04-05 13:24:24 -0700

    Updates

---
+++ precache-manifest.c2f86526d409b7d9e411703c48c1e4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1747031b11c93e5cf6cf0ab1705db5bd",
+    "url": "/homework-5/index.html"
+  },
+  {
+    "revision": "eb8ec246e9154adcb856",
+    "url": "/homework-5/static/css/main.4092cf44.chunk.css"
+  },
+  {
+    "revision": "4fb1b83ac848e413958d",
+    "url": "/homework-5/static/js/2.93f2224a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/homework-5/static/js/2.93f2224a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8ec246e9154adcb856",
+    "url": "/homework-5/static/js/main.47e59764.chunk.js"
+  },
+  {
+    "revision": "b57d1ffcc9c012666493",
+    "url": "/homework-5/static/js/runtime-main.287ea66c.js"
+  }
+]);
